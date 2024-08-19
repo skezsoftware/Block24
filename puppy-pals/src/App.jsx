68ee868d7813ca8feb815f5d1a48d78839db7814
Daniel Vasquez <dwvasquez97@gmail.com>
2024-08-19 13:37:12 -0700

    initial commit

--- puppy-pals/src/App.jsx
+++ puppy-pals/src/App.jsx
@@ -1,20 +1,22 @@
 import { useState } from 'react'
+import { puppyList } from './data'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
 
   return (
-    <>
-     
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
 
 export default App
