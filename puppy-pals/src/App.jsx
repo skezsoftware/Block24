9e93d9fdd8cb5e96e3266c4dc216f9ac4f30cc6f
Daniel Vasquez <dwvasquez97@gmail.com>
2024-08-19 18:04:41 -0700

    added CSS

--- puppy-pals/src/App.jsx
+++ puppy-pals/src/App.jsx
@@ -1,22 +1,44 @@
-import { useState } from 'react'
-import { puppyList } from './data'
-import './App.css'
+import { useState } from "react";
+import { puppyList } from "./data";
+import "./App.css";
+
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
   console.log("puppyList: ", puppyList);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+
+  
 
   return (
+    
     <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+       {featPupId && (
+        <div className="detailsOfPuppy">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
     </div>
   );
 }
 
-export default App
+export default App;
